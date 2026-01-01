ba224c5ec2d50e02f83ee3e65a34cf6ac71ef2ae
𡡚 <206096109+mknRuSp@users.noreply.github.com>
2026-01-01 21:33:35 +0900

    Add files via upload

--- OC.c
+++ OC.c
@@ -1,11 +1,19 @@
 // OC.c
-#ifndef OC
-
-int main(void){
-#define OC
+#ifndef OC1
+#define OC1
+#include <stdio.h>
 #include "OC.c"
-}
 
-#else
+#elif !OC2
+#define OC2
+int main(void)
+{
+#include "OC.c"
+#elif !OC3
+#define OC3
 printf("hello,world");
+#include "OC.c"
+#elif !OC4
+#define OC4
+}
 #endif
